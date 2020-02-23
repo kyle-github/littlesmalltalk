13938a6ae2e51e2a1ceb5a5638e96de8deb6551a
Kyle Hayes <kyle.hayes@gmail.com>
2020-02-23 15:57:41 -0800

    updated backTrace() to provide more information.   Still needs work.

--- src/vm/err.c
+++ src/vm/err.c
@@ -44,17 +44,19 @@ void backTrace(struct object * aContext)
     while (aContext && (aContext != nilObject)) {
         struct object * arguments;
         int i;
-        printf("message %s ",
-               bytePtr(aContext->data[methodInContext]
-                       ->data[nameInMethod]));
+
+        printf("message %s ", bytePtr(aContext->data[methodInContext]->data[nameInMethod]));
+
         arguments = aContext->data[argumentsInContext];
         if (arguments && (arguments != nilObject)) {
             printf("(");
-            for (i = 0; i < SIZE(arguments); i++)
+            for (i = 0; i < SIZE(arguments); i++) {
                 printf("%s%s",
                        ((i == 0) ? "" : ", "),
                        bytePtr(arguments->data[i]->class->
                                data[nameInClass]));
+            }
+
             printf(")");
         }
         printf("\n");
