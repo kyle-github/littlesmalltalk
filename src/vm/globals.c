4c852fb675402205dd86be86b84f648c0df32c0d
Kyle Hayes <kyle.hayes@gmail.com>
2020-03-01 15:33:14 -0800

    removing unneeded commented out code from old lookupGlobal.

--- src/vm/globals.c
+++ src/vm/globals.c
@@ -97,45 +97,6 @@ struct object *dictLookup(struct object *dict, char *name)
 struct object *lookupGlobal(char *name)
 {
     return dictLookup(globalsObject, name);
-//    struct object *dict;
-//    struct object *keys;
-//    struct object *key;
-//    struct object *vals;
-//    int low,high,mid;
-//    int result;
-//
-//    dict = globalsObject;
-//    keys = dict->data[0];
-//    low = 0;
-//    high = SIZE(keys);
-//
-//    /*
-//    * Do a binary search through its keys, which are Symbols.
-//    */
-//    while (low < high) {
-//        mid = (low + high) / 2;
-//        key = keys->data[mid];
-//
-//        /*
-//        * If we find the key, return the
-//        * object.
-//        */
-//
-//        result = strsymcomp(name,key);
-//
-//        if (result == 0) {
-//            vals = dict->data[1];
-//            return vals->data[mid];
-//        } else {
-//            if (result < 0) {
-//                high = mid;
-//            } else {
-//                low = mid+1;
-//            }
-//        }
-//    }
-//
-//    return 0;
 }
 
 
